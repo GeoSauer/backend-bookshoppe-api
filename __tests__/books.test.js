dba2dffc0d1a78e9298a4bddc1973b0a09638cb5
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-08 10:58:26 -0700

    add GET /books test

---
+++ __tests__/books.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /books should return a list of books', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(13);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
