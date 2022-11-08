143c5c07b431bbf2a9032fcfff4718b6a30521af
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-07 18:14:55 -0700

    add GET /authors test

---
+++ __tests__/authors.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('authors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /authors should return a list of authors', async () => {
+    const resp = await request(app).get('/authors');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(3);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
